7791bd43b1621333bfe144bb1b0bedbf2ace95b4
Yazil Ansari <yazilansari@gmail.com>
2025-01-30 10:45:42 +0400

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,27 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+ 
+const withNextIntl = createNextIntlPlugin();
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: false,
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'phpstack-1403159-5212295.cloudwaysapps.com',
+          },
+          {
+            protocol: 'https',
+            hostname: 'adminksa.ahmed-perfume.com',
+          },
+          {
+            protocol: 'http',
+            hostname: 'localhost',
+          }
+        ],
+      },
+      productionBrowserSourceMaps: true,
+      // basePath: '/ksa'
+};
+
+export default withNextIntl(nextConfig);
