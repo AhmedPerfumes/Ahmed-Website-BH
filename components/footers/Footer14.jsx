693907490d9a9cbd5a0311e1d4435d59ec80eb91
“Arshad <arshad@ahmedalmaghribi.com>
2025-05-21 15:22:20 +0400

    added wedding and edited links

--- components/footers/Footer14.jsx
+++ components/footers/Footer14.jsx
@@ -125,13 +125,13 @@ export default function Footer14() {
           <div className="footer-column footer-store-info col-12 mb-4 mb-lg-0">
             <div className="logo">
               <a href="/">
-              <Image
-                  src="/assets/images/about/ahmed-logo.png"
-                  width={100}
-                  height={100}
-                  alt="Ahmed"
-                  className="logo__image d-block"
-                />
+                <Image
+                    src="/assets/images/about/ahmed-logo.png"
+                    width={100}
+                    height={100}
+                    alt="Ahmed"
+                    className="logo__image d-block"
+                  />
               </a>
             </div>
             {/* <!-- /.logo --> */}
