e4ee08593aeb9129539bd7f31db33e95a580ce58
“Arshad <arshad@ahmedalmaghribi.com>
2025-09-30 18:49:55 +0400

    added analytics gtag tiktok pixel configured

--- app/[locale]/layout.jsx
+++ app/[locale]/layout.jsx
@@ -74,7 +74,7 @@ export default async function LocaleLayout({ children, params: { locale } }) {
         <html lang={locale} dir={locale === "ar" ? "rtl" : "ltr"}>
            
             <body className={selectedFont.className}>
-            <Script id="gtm-script" strategy="afterInteractive">
+        <Script id="gtm-script" strategy="afterInteractive">
           {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -83,7 +83,18 @@ export default async function LocaleLayout({ children, params: { locale } }) {
             })(window,document,'script','dataLayer','${GTM_ID}');
           `}
         </Script>
-            <noscript>
+
+        <Script id="tiktok-pixel" strategy="afterInteractive">
+          {`
+            !function (w, d, t) {
+              w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script");n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+              ttq.load('D3D8RK3C77UF12B9UI50');
+              ttq.page();
+            }(window, document, 'ttq');
+          `}
+        </Script>
+
+        <noscript>
           <iframe
             src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
             height="0"
