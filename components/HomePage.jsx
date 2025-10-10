d68d1b7b1d4f82684e74e66f33c0461d5a9ab1f7
Saad Edroos <saadedroossaad@gmail.com>
2025-10-10 12:33:57 +0400

    Fixed banner and header

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -152,7 +152,7 @@ const ScrollSnapHorizontalBootstrap = () => {
             <section className="vh-100 hero-banner d-none d-lg-block">
                 <Hero />
             </section>
-            <div className="d-block d-lg-none mt-4">
+            <div className="d-block d-lg-none">
                 <Hero2 />
             </div>
 
