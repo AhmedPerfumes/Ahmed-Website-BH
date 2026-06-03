37a315ae5e7bfda838e2b4dd7c45a668f841cae7
Saad Edroos <saadedroossaad@gmail.com>
2026-06-03 11:50:17 +0400

    fixed label

--- components/shoplist/shop10/Style2.jsx
+++ components/shoplist/shop10/Style2.jsx
@@ -254,7 +254,9 @@ export default function Style2({ category, subcategory, products: initialProduct
                         <div className="product-label label--out-of-stock">{t("Out Of Stock")}</div>
                       ) : (
                         elm.discount && (
-                          <div className="product-label label--sale">{t("Sale")} {elm.discount.value}%</div>
+                          <div style={{ backgroundColor: '#198754' }} className="product-label text-uppercase text-white top-0 left-0 mt-2 mx-2">
+                            {elm.discount.discount_type === "percent" ? `Sale ${elm.discount.value}%` : "Sale"}
+                          </div>
                         )
                       )}
                       </SwiperSlide>
